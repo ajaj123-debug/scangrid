7be2aa62e6fc6adc50b0677b09f8cd028f4b630c
ajaj123-debug <ajaj42699@gmail.com>
2026-05-20 01:50:29 +0530

    commit

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,6 +11,7 @@
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as IndexRouteImport } from './routes/index'
 
+
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
